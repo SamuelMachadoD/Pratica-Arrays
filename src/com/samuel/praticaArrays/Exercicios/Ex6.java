22ae511cd6412b31edca6b619f5629a292d66b40
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-15 17:58:51 -0300

    Exercicio 6
    6. Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
    um vetor C, onde cada elemento de C é a soma dos respectivos
    elementos em A e B, ou seja:
    C[i] = A[i] + B[i].

---
+++ src/com/samuel/praticaArrays/Exercicios/Ex6.java
@@ -0,0 +1,40 @@
+package com.samuel.praticaArrays.Exercicios;
+
+import java.util.Scanner;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] a = new int[10];
+		int[] b = new int[a.length];
+		int[] c = new int[a.length];
+		
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("Digite um valor: ");
+			a[i] = scan.nextInt();
+			b[i] = a[i] * i;
+			c[i] = a[i] + b[i];
+		}
+		
+		System.out.println("A: ");
+		for(int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+		System.out.println("B: ");
+		for(int i = 0; i < b.length; i++) {
+			System.out.print(b[i] + " ");
+		}
+		System.out.println();
+		System.out.println("C: ");
+		for(int i = 0; i < c.length; i++) {
+			System.out.print(c[i] + " ");
+		}
+
+	}
+
+}
