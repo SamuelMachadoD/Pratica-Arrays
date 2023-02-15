cf912d9bbb6182e5acb1df6bc67aba7c92ca8a48
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-15 17:26:49 -0300

    Exercício 1
    1. Criar um vetor A com 5 elementos inteiros. Construir um vetor B de
    mesmo tipo e tamanho e com os "mesmos" elementos do vetor A, ou
    seja, B[i] = A[i].

--- src/com/samuel/praticaArrays/Exercicios/Ex1.java
+++ src/com/samuel/praticaArrays/Exercicios/Ex1.java
@@ -1,9 +1,32 @@
 package com.samuel.praticaArrays.Exercicios;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] a = new int[5];
+		int[] b = new int[a.length];
+		
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("Digite um valor: ");
+			a[i] = scan.nextInt();
+			b[i] = a[i];
+		}
+		
+		System.out.println("A: ");
+		for(int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+		System.out.println("B: ");
+		for(int i = 0; i < b.length; i++) {
+			System.out.print(b[i] + " ");
+		}
 
 	}
 
