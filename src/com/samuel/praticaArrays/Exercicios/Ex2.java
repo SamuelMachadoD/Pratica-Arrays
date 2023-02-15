e8a2f924446106b3e5f998d30cdfe717018743bc
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-15 17:28:37 -0300

    Questao 2
    2. Criar um vetor A com 8 elementos inteiros. Construir um vetor B de
    mesmo tipo e tamanho e com os elementos do vetor A multiplicados
    por 2, ou seja: B[i] = A[i] * 2.

---
+++ src/com/samuel/praticaArrays/Exercicios/Ex2.java
@@ -0,0 +1,33 @@
+package com.samuel.praticaArrays.Exercicios;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] a = new int[8];
+		int[] b = new int[a.length];
+		
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("Digite um valor: ");
+			a[i] = scan.nextInt();
+			b[i] = a[i] * 2;
+		}
+		
+		System.out.println("A: ");
+		for(int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+		System.out.println("B: ");
+		for(int i = 0; i < b.length; i++) {
+			System.out.print(b[i] + " ");
+		}
+
+	}
+
+}
