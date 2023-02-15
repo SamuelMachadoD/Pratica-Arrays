6c01780f368b3996924cab8ba229386e9fabfd0e
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-15 19:06:31 -0300

    Exercicio 1
    1. Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-
    9. Após isso determine o maior número da matriz e a sua posição
    (linha, coluna

--- src/com/samuel/praticaArrays/Matrizes/Exercicios/Ex01.java
+++ src/com/samuel/praticaArrays/Matrizes/Exercicios/Ex01.java
@@ -1,9 +1,34 @@
 package com.samuel.praticaArrays.Matrizes.Exercicios;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Random random = new Random();
+		int linhaMaior = 0;
+		int colunaMaior = 0;
+		int maior = 0;
+		
+		int[][] matriz = new int[4][4];
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz.length; j++) {
+				matriz[i][j] = random.nextInt(10);
+				System.out.print(matriz[i][j] + " ");
+				if(matriz[i][j] > maior) {
+					colunaMaior = j;
+					linhaMaior = i;
+					maior = matriz[i][j];
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("O maior numero é : " + maior);
+		System.err.println("Coluna: " + colunaMaior + " Linha: " + linhaMaior);
 
 	}
 
