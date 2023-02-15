c346517044a8da38366c640a95becdc99cca87b6
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-15 17:46:05 -0300

    Exercício 5
    5. Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
    mesmo tipo e tamanho, sendo que cada elemento do vetor B deverá
    ser o respectivo elemento de A multiplicado por sua posição (ou
    índice), ou seja:
    B[i] = A[i] * i.

--- src/com/samuel/praticaArrays/Exercicios/Ex5.java
+++ src/com/samuel/praticaArrays/Exercicios/Ex5.java
@@ -8,14 +8,14 @@ public class Ex5 {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int[] a = new int[15];
+		int[] a = new int[10];
 		int[] b = new int[a.length];
 		
 		
 		for(int i = 0; i < a.length; i++) {
 			System.out.println("Digite um valor: ");
 			a[i] = scan.nextInt();
-			b[i] = a[i] * a[i];
+			b[i] = a[i] * i;
 		}
 		
 		System.out.println("A: ");
